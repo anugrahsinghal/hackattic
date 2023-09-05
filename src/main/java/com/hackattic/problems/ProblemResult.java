efc13b51fa4d923873fd476407d865ec4f0fb8cb
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-09-05 23:52:02 +0530

    some extra ProblemResult fields for diff cases

--- src/main/java/com/hackattic/problems/ProblemResult.java
+++ src/main/java/com/hackattic/problems/ProblemResult.java
@@ -1,8 +1,22 @@
 package com.hackattic.problems;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.ToString;
 
+@ToString
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProblemResult {
-    private String result;
+    @JsonProperty("result")
+    private String _result;
+    private String rejected;
+    private String message;
+    private String hint;
+
+
+    public String result() {
+        return "\n\n"+ this;
+    }
 }
